44d1adb33ecb95d2be0f11f5d9ea0bfdc0cb861b
Michael Evans <michaelcevans10@gmail.com>
2015-11-13 00:02:37 -0500

    initial commit

---
+++ dexter/src/androidTest/java/org/michaelevans/dexter/ApplicationTest.java
@@ -0,0 +1,35 @@
+package org.michaelevans.dexter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.test.InstrumentationTestCase;
+import java.util.List;
+
+public class ApplicationTest extends InstrumentationTestCase {
+
+  public void testReceiverRegistered() throws Exception {
+    Intent intent = new Intent(Intent.ACTION_MY_PACKAGE_REPLACED);
+
+    Context context = getInstrumentation().getTargetContext();
+
+    List<ResolveInfo> list = context.getPackageManager().queryBroadcastReceivers(intent, 0);
+
+    boolean receiverRegistered = false;
+
+    for (ResolveInfo resolveInfo : list) {
+      if (resolveInfo.activityInfo.name.equals(NoopBroadcastReceiver.class.getName())) {
+        receiverRegistered = true;
+        break;
+      }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      assertFalse(receiverRegistered);
+    } else {
+      assertTrue(receiverRegistered);
+    }
+  }
+}
+
---
+++ dexter/src/main/java/org/michaelevans/dexter/NoopBroadcastReceiver.java
@@ -0,0 +1,12 @@
+package org.michaelevans.dexter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NoopBroadcastReceiver extends BroadcastReceiver {
+
+  @Override public void onReceive(Context context, Intent intent) {
+    //No-op!
+  }
+}
